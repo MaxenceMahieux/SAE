8cc9fc79f98b070212dfb9f27561bcdb4fa69bfe
Maxence Mahieux <pro.maxencemahieux@gmail.com>
2024-01-24 15:59:00 +0100

    Modification disposition après changement Navbar

--- pages/rules.js
+++ pages/rules.js
@@ -1,6 +1,12 @@
+import Navbar from "@/components/Navbar";
+
 export default function Rules() {
     return  (
-        <>
-        </>
+        <div className="Rules">
+            <Navbar />
+            <main className="pt-20">
+
+            </main>
+        </div>
     )
 }
