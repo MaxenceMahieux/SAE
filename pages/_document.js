7df060609c69c0e20fd4e139bf726741ae7c5b1d
Maxence Mahieux <pro.maxencemahieux@gmail.com>
2024-01-24 15:57:47 +0100

    Intrégration SEO

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="fr">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="fr">
+            <Head />
+            <link rel="icon" href="/img/Favicon.ico" type="image/x-icon" />
+            <meta name="keywords" content="San, Andreas, Emergency, FivePD, ToKu, FiveM, Maxence, Mahieux" />
+            <meta name="description" content="Explorez un San Andreas réaliste sur notre serveur FiveM. Vivez des aventures en tant que policier, pompier, ou citoyen, et forgez votre destinée dans un monde dynamique."/>
+            <title>San Andreas Emergency - FivePD</title>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
