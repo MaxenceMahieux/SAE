a78f70a478757a7000bf0b36dea96d2e1a794b77
Maxence Mahieux <pro.maxencemahieux@gmail.com>
2024-01-25 18:57:45 +0100

    Création page TOS & Legals

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
             <div className="flex justify-between p-7 md:px-10 lg:px-40 py-5 font-clash">
                 <p className="text-white/50 text-sm">Copyright © 2023 <Link href="https://maxmhx.fr" target="_blank" className="duration-200 hover:duration-200 hover:text-white cursor-pointer">Maxence Mahieux</Link> </p>
                 <div className="flex gap-8">
-                    <Link href='/legal' className="text-white/50 text-sm duration-200 hover:duration-200 hover:text-white cursor-pointer">Mentions légales</Link>
+                    <Link href='/legals' className="text-white/50 text-sm duration-200 hover:duration-200 hover:text-white cursor-pointer">Mentions légales</Link>
                     <Link href='/tos' className="text-white/50 text-sm duration-200 hover:duration-200 hover:text-white cursor-pointer">Conditions d'utilisations</Link>
                 </div>
             </div>
