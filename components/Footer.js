984ab68835664226084159e261acf1beee16cf37
Maxence Mahieux <pro.maxencemahieux@gmail.com>
2024-01-29 16:11:15 +0100

    Ajout paramètre " Glue " Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
-export default function Footer() {
+export default function Footer({glue}) {
     return (
-        <footer className="absolute bottom-0">
+        <footer className={`${ glue ? 'absolute bottom-0' : '' }`}>
             <div className="separator"></div>
             <div className="flex justify-between p-7 md:px-10 lg:px-40 py-5 font-clash">
                 <p className="text-white/50 text-sm">Copyright © 2023 <Link href="https://maxmhx.fr" target="_blank" className="duration-200 hover:duration-200 hover:text-white cursor-pointer">Maxence Mahieux</Link> </p>
